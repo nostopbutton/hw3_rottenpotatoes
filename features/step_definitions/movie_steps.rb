e18e9129ed02075d843ab8d965caf9315eba96ce
nostopbutton <pete@nostopbutton.com>
2013-08-11 17:08:49 -0700

    hw3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,15 @@
 # Add a declarative step here for populating the DB with movies.
+number_of_movies = 0
 
 Given /the following movies exist/ do |movies_table|
+  number_of_movies = 0
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+     Movie.create(movie)
+     number_of_movies+=1
   end
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.should =~ /#{e1}.*#{e2}/m
+  # flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +30,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(', ').each do | rating |
+    if uncheck 
+      step "I uncheck \"ratings_#{rating}\""
+    else
+      step "I check \"ratings_#{rating}\""
+    end
+  end
+  # all("table#movies tr").size.should == number_of_movies
+end
+
+Then /I should see all of the movies/ do
+  all("table#movies tr").size.should == number_of_movies+1
 end
